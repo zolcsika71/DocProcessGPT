7f637807b86b8f31ca8d1b6c1550490d41a74bf7
zolcsika71 <bushdoctor2008@gmail.com>
2024-10-06 16:03:04 +0000

    replit update

--- replit.nix
+++ replit.nix
@@ -12,14 +12,3 @@
   ];
 }
 
-{
-  environment.systemPackages = with pkgs; [
-    glibcLocales
-  ];
-
-  environment.variables = {
-    LANG = "en_AU.UTF-8";
-    LC_ALL = "en_AU.UTF-8";
-  };
-}
-
