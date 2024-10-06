1a81c26304306e4d419f5078eb5cee782fcf8f23
zolcsika71 <bushdoctor2008@gmail.com>
2024-10-06 18:07:38 +0200

    Change Flask app port from 5000 to 5001
    Updated the port for the Flask application in both main.py and app.py to address potential conflicts or requirements. This ensures the application runs on port 5001 instead of 5000.

--- app.py
+++ app.py
@@ -434,5 +434,4 @@ def handle_exception(e):
 
 if __name__ == "__main__":
     download_nltk_resources()
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5001, debug=True)
--- main.py
+++ main.py
@@ -11,4 +11,4 @@ from app import app
 
 if __name__ == "__main__":
     print("About to start Flask app...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
